69f926918412b1d2975576c85c13014aebf75d0e
lomen <lomen0857@gmail.com>
2019-06-11 21:28:39 +0900

    edit import

--- importTrainData.py
+++ importTrainData.py
@@ -1,8 +1,15 @@
+#%%
 import numpy as np
 import pandas as pd
 
+db_genba = pd.read_csv("train_genba.tsv",encoding="utf8",sep="\t")
+db_goto = pd.read_csv("train_goto.tsv",encoding="utf8",sep="\t")
+
 #%%
-a = 1
-b = 2
-c = a + b + 4
+db = pd.merge(db_genba, db_goto, on='pj_no', how='left')
+
+#%%
+Y = db["keiyaku_pr"]
+X = db.drop("keiyaku_pr",axis=1)
+
 #%%
